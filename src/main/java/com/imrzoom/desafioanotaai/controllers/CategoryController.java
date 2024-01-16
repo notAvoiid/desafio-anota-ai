670035af364e990d74d92c64aca3b13cc9753a64
Igor <igor.nacimento51@gmail.com>
2024-01-16 12:29:12 -0300

    "Adding swagger and documenting application"

--- src/main/java/com/imrzoom/desafioanotaai/controllers/CategoryController.java
+++ src/main/java/com/imrzoom/desafioanotaai/controllers/CategoryController.java
@@ -3,6 +3,12 @@ package com.imrzoom.desafioanotaai.controllers;
 import com.imrzoom.desafioanotaai.domain.category.Category;
 import com.imrzoom.desafioanotaai.domain.category.dto.CategoryDTO;
 import com.imrzoom.desafioanotaai.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/category")
+@Tag(name = "Category", description = "Endpoint for managing your Category")
 public class CategoryController {
 
     private final CategoryService service;
@@ -19,31 +26,81 @@ public class CategoryController {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<Category> create(@RequestBody CategoryDTO categoryDTO){
-        Category newCategory = this.service.create(categoryDTO);
-        return ResponseEntity.ok().body(newCategory);
-    }
-
     @GetMapping
+    @Operation(summary = "List all categories!", description = "List all categories!",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Category.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     public ResponseEntity<List<Category>> findAll(){
         List<Category> newCategory = this.service.findAll();
         return ResponseEntity.ok().body(newCategory);
     }
 
     @GetMapping("/{id}")
+    @Operation(operationId = "id",summary = "Finds a category!", description = "Finds a category!",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = Category.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Optional<Category>> findById(@PathVariable String id){
         Optional<Category> newCategory = this.service.findById(id);
         return ResponseEntity.ok().body(newCategory);
     }
 
+    @PostMapping
+    @Operation(summary = "Creates a category!", description = "Creates a category!",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = Category.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
+    public ResponseEntity<Category> create(@RequestBody CategoryDTO categoryDTO){
+        Category newCategory = this.service.create(categoryDTO);
+        return ResponseEntity.ok().body(newCategory);
+    }
+
     @PutMapping("/{id}")
+    @Operation(operationId = "id",summary = "Updates a category!", description = "Updates a category!",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = Category.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryDTO) {
         Category updatedCategory = this.service.update(id, categoryDTO);
         return ResponseEntity.ok().body(updatedCategory);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(operationId = "id",summary = "Deletes a category!", description = "Deletes a category!",
+            tags = {"Category"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204",
+                            content = @Content(schema = @Schema(implementation = Category.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Category> delete(@PathVariable("id") String id) {
         this.service.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/imrzoom/desafioanotaai/controllers/ProductController.java
+++ src/main/java/com/imrzoom/desafioanotaai/controllers/ProductController.java
@@ -3,7 +3,12 @@ package com.imrzoom.desafioanotaai.controllers;
 import com.imrzoom.desafioanotaai.domain.product.Product;
 import com.imrzoom.desafioanotaai.domain.product.dto.ProductDTO;
 import com.imrzoom.desafioanotaai.services.ProductService;
-import jakarta.websocket.server.PathParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/product")
+@Tag(name = "Product", description = "Endpoint for managing your Product")
 public class ProductController {
 
     private final ProductService service;
@@ -21,30 +27,80 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(summary = "List all products!", description = "List all products!",
+            tags = {"Product"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Product.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     public ResponseEntity<List<Product>> findAll(){
         List<Product> newProduct = this.service.findAll();
         return ResponseEntity.ok().body(newProduct);
     }
 
     @GetMapping("/{id}")
+    @Operation(operationId = "id",summary = "Finds a product!", description = "Finds a product!",
+            tags = {"Product"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Optional<Product>> findById(@PathVariable String id){
         Optional<Product> newProduct = this.service.findById(id);
         return ResponseEntity.ok().body(newProduct);
     }
 
     @PostMapping
+    @Operation(summary = "Creates a product!", description = "Creates a product!",
+            tags = {"Product"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     public ResponseEntity<Product> create(@RequestBody ProductDTO productDTO){
         Product newProduct = this.service.create(productDTO);
         return ResponseEntity.ok().body(newProduct);
     }
 
     @PutMapping("/{id}")
+    @Operation(operationId = "id",summary = "Updates a product!", description = "Updates a product!",
+            tags = {"Product"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content (schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Product> update(@PathVariable("id") String id, @RequestBody ProductDTO productDTO) {
         Product updatedProduct = this.service.update(id, productDTO);
         return ResponseEntity.ok().body(updatedProduct);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(operationId = "id",summary = "Deletes a product!", description = "Deletes a product!",
+            tags = {"Product"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204",
+                            content = @Content(schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Product> delete(@PathVariable("id") String id) {
         this.service.delete(id);
         return ResponseEntity.noContent().build();
